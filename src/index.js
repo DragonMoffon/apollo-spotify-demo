adc9bd1af04ff1ab28960bdf68af3d48e65def00
Aedan Andrews <happydog.aja@gmail.com>
2023-11-30 12:03:23 +1300

    Query Working

--- src/index.js
+++ src/index.js
@@ -10,14 +10,16 @@ import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
 import { SPF_search_resolvers } from './spotify/search.js'
 import { get_access_token, AccessToken } from './spotify/auth.js'
 
-const typeDefs = await loadSchema( './**/*.graphql',  { loaders: [new GraphQLFileLoader()]} );
+const typeDefs = await loadSchema( './**/compressed_schema.graphql',  { loaders: [new GraphQLFileLoader()]} );
 
 const server = new ApolloServer(
     {
         typeDefs,
-        resolvers
+        resolvers: SPF_search_resolvers
     }
 );
+
+console.log(SPF_search_resolvers)
 
 const { url } = await startStandaloneServer(server, {
     listen: {port: 4000}
@@ -27,4 +29,4 @@ const next_access_token = await get_access_token()
 AccessToken.set_token(next_access_token.token, next_access_token.expires)
 
 console.log(`🚀  Server ready at: ${url}`)
-console.log(AccessToken.get_token)
+console.log(AccessToken.get_token())
