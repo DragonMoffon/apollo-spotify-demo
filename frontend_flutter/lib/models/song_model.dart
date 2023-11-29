3e1fbc46051cfae6a0dd30ad914fa30d9a286288
Rachel Nataatmadja <nataatmadjarachel@gmail.com>
2023-11-29 14:47:43 +1300

    init flutter files

---
+++ frontend_flutter/lib/models/song_model.dart
@@ -0,0 +1,11 @@
+class SongModel {
+  final String title;
+  final List<String> artist;
+  final int length;
+
+  SongModel({
+    required this.title,
+    required this.artist,
+    required this.length,
+  });
+}
