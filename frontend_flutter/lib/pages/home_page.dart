dede4943d6089872e097fe2a1d0e85793d52201e
Rachel Nataatmadja <nataatmadjarachel@gmail.com>
2023-12-01 11:25:22 +1300

    Added search options and styled search bar and search results

--- frontend_flutter/lib/pages/home_page.dart
+++ frontend_flutter/lib/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:frontend_flutter/models/SPF_track_model.dart';
 import 'package:frontend_flutter/services/graphql_service.dart';
 
+const List<String> searchOptions = ["track", 'artist'];
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -14,7 +16,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   List<SPF_TrackModel>? _tracks;
   final GraphQLService _graphQLService = GraphQLService();
-  final textController = TextEditingController();
+  final TextEditingController textController = TextEditingController();
+  final List<bool> _toggleSearchSelection = searchOptions
+      .map((e) => e == "track")
+      .toList(); // map out default search selection states
 
   @override
   void initState() {
@@ -22,8 +27,27 @@ class _MyHomePageState extends State<MyHomePage> {
     _tracks = null;
   }
 
+  // checks which search functionality to do based on search options
+  void _runSearch(String name) {
+    int selectedIndex =
+        _toggleSearchSelection.indexWhere((isSelected) => isSelected);
+
+    if (selectedIndex != -1) {
+      String selectedOption = searchOptions[selectedIndex];
+      print('Selected Search Option: $selectedOption');
+
+      // run the query that satisfy the filters/ search selection
+      if (selectedOption == 'track' && selectedOption == 'artist') {
+        print("both selected");
+      } else if (selectedOption == 'track') {
+        print("track selected getting tracks...");
+        _getTrack(name);
+      }
+    }
+  }
+
   // Gets tracks from spotify API
-  void _getTrack(title) async {
+  void _getTrack(String title) async {
     _tracks = await _graphQLService.getTrackFromSearch(inputTitle: title);
     setState(() {});
   }
@@ -36,47 +60,82 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child:FractionallySizedBox(
+        child: FractionallySizedBox(
           widthFactor: 0.8,
-          child:Column(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              const Padding(padding: EdgeInsets.all(2.0)),
-              TextField(
-                onSubmitted: (text) {
-                  _getTrack(text);
-                },
-                controller: textController,
-                decoration: InputDecoration(
-                  border: const OutlineInputBorder(),
-                  hintText: 'Search for a Track',
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      _getTrack(textController.text);
-                    },
-                    icon: const Icon(
-                      Icons.search,
+              const Padding(padding: EdgeInsets.all(5.0)),
+              Material(
+                shadowColor: Colors.grey,
+                elevation: 5.0,
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
+                child:TextField(
+                  onSubmitted: (text) {
+                    _runSearch(text);
+                  },
+                  controller: textController,
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      borderSide: BorderSide.none,
+                    ),
+                    fillColor: Colors.white,
+                    hintText: 'Search for a Track...',
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        _runSearch(textController.text);
+                      },
+                      icon: const Icon(
+                        Icons.search,
+                      ),
                     ),
                   ),
                 ),
               ),
 
+              const Padding(padding: EdgeInsets.all(2.0)),
+              // ---- Search filters ----
+              Material(
+                shadowColor: Colors.grey,
+                elevation: 2.0,
+                borderRadius: const BorderRadius.all(Radius.circular(8)),
+                child:ToggleButtons(
+                  isSelected: _toggleSearchSelection,
+                  onPressed: (index) {
+                    setState(() {
+                      _toggleSearchSelection[index] =
+                          !_toggleSearchSelection[index];
+                    });
+                  },
+                  borderRadius: const BorderRadius.all(Radius.circular(8)),
+                  children: List<Widget>.generate(
+                    searchOptions.length,
+                    (index) => Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Text(searchOptions[index]),
+                    ),
+                  ),
+                ),
+              ),
+              const Padding(padding: EdgeInsets.all(5.0)),
+              // ---- Search Results ----
               // shows search items in a list view
-              _tracks == null
-                ? const Text('')
-                : Expanded(
-                  child: ListView.builder(
-                    itemCount: _tracks?.length,
-                    shrinkWrap: true,
-                    itemBuilder: (BuildContext context, int index) {
-                      return ListTile(
+              _tracks == null? const Text('') : Expanded(
+                child: ListView.builder(
+                  itemCount: _tracks?.length,
+                  shrinkWrap: true,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      child: ListTile(
                         leading: const Icon(Icons.music_note),
                         title: Text('${_tracks?[index].name}'),
                         trailing: Text('${_tracks?[index].duration}'),
-                      );
-                    },
-                  ),
+                      ),
+                    );
+                  },
                 ),
+              ),
             ],
           ),
         ),
